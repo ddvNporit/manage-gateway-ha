bae7bdc58b5be8e9bb9f82abccc55c0cbc13ac33
ddv <dima.nporit@gmail.com>
2025-12-23 08:43:11 +0300

    first commit #3

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Router, Bot
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Text
+from request_comand import RequestApi
 
 router: Router = Router()
 
@@ -12,4 +13,12 @@ async def process_start_comand(message: Message):
 
 @router.message(Text(text='Hi, Bot'))
 async def hi(message: Message, bot: Bot):
-    await bot.send_message(message.from_user.id, f"hi, {message.from_user.full_name}")
+    await (bot.send_message
+           (message.from_user.id, f"hi, {message.from_user.full_name} ({message.from_user.id})"))
+
+
+@router.message(Text(text='config HA'))
+async def get_config_ha(message: Message, bot: Bot):
+    req_ha = RequestApi()
+    code, response = req_ha.method_get("config", None)
+    await bot.send_message(message.from_user.id, f"ответ: {response.text}")
--- request_comand.py
+++ request_comand.py
@@ -8,13 +8,7 @@ class RequestApi:
         config_app: Config = load_config()
         url = f"{config_app.ha.url}/api/{name_method_ha}"
         headers = {"Authorization": f"Bearer {config_app.ha.token}", "content-type": "application/json"}
-        return get(url, params=params_request, headers=headers)
 
-
-config: Config = load_config()
-name_method = "config"
-token = config.ha.token
-params = {}
-r_api = RequestApi()
-response = r_api.method_get(name_method_ha=name_method, params_request=params)
-print(response.text)
+        response = get(url, params=params_request, headers=headers)
+        code = response.status_code
+        return code, response
