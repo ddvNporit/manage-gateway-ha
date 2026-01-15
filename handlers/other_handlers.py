07f34ee236dd4923094bc4cccc59ef18ca72dbd0
ddv <dima.nporit@gmail.com>
2026-01-15 13:03:53 +0300

    функция получения массива изменений состояния

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -9,8 +9,8 @@ from aiogram.types import Message, FSInputFile
 
 from config_data.config import Config, load_config
 from handlers.commands import CommandsOnHa as Com_Ha
-from handlers.data_processor import FiltersData as Fp
 from handlers.data_processor import DateTimeProcessor
+from handlers.data_processor import FiltersData as Fp
 from request_comand import RequestApi
 
 router: Router = Router()
@@ -89,7 +89,7 @@ async def get_states_ha(message: Message, bot: Bot):
     if not await check_access(message):
         return
 
-    filter_value, value, atr = await Fp.processing_filter(message)
+    filter_value, value, atr = await _get_filter(message)
 
     req_ha = RequestApi()
     code, response = req_ha.method_get("states", None)
@@ -139,7 +139,7 @@ registered_commands.update(["<code>update:bool states</code> - обновить 
 async def updated_states_ha(message: Message, bot: Bot):
     if not await check_access(message):
         return
-    field, value, atr = await Fp.processing_filter(message, 3)
+    field, value, atr = await _get_filter(message)#= await Fp.processing_filter(message, 3)
     if value in ('1', 'on', 'вкл'):
         value = 'on'
     elif value in ('0', 'off', 'выкл'):
@@ -174,10 +174,7 @@ registered_commands.update(["<code>turn ha entity_id X</code> - вкл/выкл 
 async def turn_ha_object(message: Message, bot: Bot, cmd: str = None):
     if not await check_access(message):
         return
-    if cmd is None:
-        field, value, atr = await Fp.processing_filter(message, 3)
-    else:
-        field, value, atr = await Fp.processing_short_filter(cmd, 3)
+    field, value, atr = await _get_filter(message, cmd, 3)
     if value in ('1', 'on', 'вкл'):
         value = 'on'
     elif value in ('0', 'off', 'выкл'):
@@ -221,49 +218,41 @@ registered_commands.update(
 
 
 @router.message(lambda message: message.text.lower().startswith("history"))
-async def get_history(message: Message, bot: Bot, cmd: str = None):
+async def get_history(message: Message, bot: Bot, cmd: str = None) -> None:
     if not await check_access(message):
         return
-    if cmd is None:
-        filter_value, value, atr = await Fp.processing_filter(message, 3)
-    else:
-        filter_value, value, atr = await Fp.processing_short_filter(cmd, 3)
-    if atr is not None:
 
-        param = {
-            'filter_entity_id': atr
-        }
-    else:
-        await bot.send_message(message.from_user.id,
-                               f"Запрос отклонен. Пустой entity_id")
+    filter_value, value, atr = await _get_filter(message, cmd, 3)
+    if not atr:
+        await bot.send_message(message.from_user.id, "Запрос отклонен. Пустой entity_id")
         return
+
+    param = {'filter_entity_id': atr}
     req_ha = RequestApi()
-    if filter_value is not None:
-        try:
-            delta_int = int(filter_value)
-            if 7 >= delta_int > 0:
-                delta_data = DateTimeProcessor.get_date_minus_delta(delta_int)
-                await bot.send_message(message.from_user.id,
-                                       f"{atr}: чтение массива изменений состояния до {delta_data}...")
-                code, response = req_ha.method_get(f"history/period/{delta_data}", param)
-            else:
-                await bot.send_message(message.from_user.id,
-                                       f"Проверьте корректность запроса, некорректное значение delta (X)")
-                return
-        except (TypeError, ValueError):
+
+    delta_date = None
+    if filter_value:
+        if not _validate_delta(filter_value):
             await bot.send_message(message.from_user.id,
-                                   f"Проверьте корректность запроса, некорректное значение delta (X)")
+                                   "Проверьте корректность запроса, некорректное значение delta (filter)")
             return
+        delta_date = DateTimeProcessor.get_date_minus_delta(int(filter_value))
+        await (bot.send_message
+               (message.from_user.id, f"{atr}: чтение массива изменений состояния с {delta_date}..."))
+
     else:
-        await bot.send_message(message.from_user.id,
-                               f"{atr}: чтение массива изменений состояния за сутки...")
-        code, response = req_ha.method_get(f"history/period", param)
+        await (bot.send_message
+               (message.from_user.id, f"{atr}: чтение массива изменений состояния за сутки..."))
+
+    endpoint = f"history/period/{delta_date}" if delta_date else "history/period"
+    code, response = req_ha.method_get(endpoint, param)
+
     if int(code) != 200:
         await bot.send_message(message.from_user.id,
                                f"код ответа: {code}, проверьте работоспособность HA, и корректность запроса")
         return
-    else:
-        await bot.send_message(message.from_user.id, f"ответ: {response.text}")
+
+    await bot.send_message(message.from_user.id, f"ответ: {response.text}")
 
 
 registered_commands.update(
@@ -276,10 +265,7 @@ registered_commands.update(
 async def get_security_camera_image(message: Message, bot: Bot, cmd: str = None):
     if not await check_access(message):
         return
-    if cmd is None:
-        filter_value, value, atr = await Fp.processing_filter(message)
-    else:
-        filter_value, value, atr = await Fp.processing_short_filter(cmd)
+    filter_value, value, atr = await _get_filter(message, cmd, 3)
     if filter_value is not None:
         req_ha = RequestApi()
         code, response = req_ha.method_get(f"camera_proxy/camera.{filter_value}", None)
@@ -345,3 +331,18 @@ async def answer_updated_states(bot, code, message, response):
     else:
         await bot.send_message(message.from_user.id,
                                f"код ответа: {code}, проверьте работоспособность HA, и корректность запроса")
+
+
+async def _get_filter(message: Message, cmd: str = None, count_separators=2):
+    if cmd is None:
+        return await Fp.processing_filter(message, count_separators)
+    else:
+        return await Fp.processing_short_filter(cmd, count_separators)
+
+
+def _validate_delta(value: str) -> bool:
+    try:
+        val = int(value)
+        return 0 < val <= 7
+    except (TypeError, ValueError):
+        return False
